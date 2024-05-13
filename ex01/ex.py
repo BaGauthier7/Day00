06268ad3e9def7c855517e67957956bf477d2e01
BaGauthier7 <148325497+BaGauthier7@users.noreply.github.com>
2024-05-13 12:34:56 +0200

    Create ex.py

---
+++ ex01/ex.py
@@ -0,0 +1,7 @@
+
+def miles_to_km():
+    distance_miles = float(input("Entrez la distance en miles : "))
+    distance_km = distance_miles * 1.60934
+    print(f"{distance_miles} miles équivaut à {distance_km} kilomètres.")
+
+miles_to_km()
